d3f5907654e501fb45f8ed5336cb293e120c8085
Will Gundy <willgunderson503@gmail.com>
2022-05-05 09:40:17 -0700

    added comments for each outlined component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { useState, useEffect } from 'react';
 
 function App() {
+  //state to be tracked in app.js
   const [movies, setMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState([]);
   const [formTitle, setFormTitle] = useState('');
@@ -12,11 +13,26 @@ function App() {
 
   return (
     <div className="App">
+
+      {/*top section where the user can add/edit movie information*/}
       <section className='addMovie'>
+
+        {/*form for adding a new movie to the Movie List, where state is primarily edited*/}
         <MovieForm />
+
+        {/*Object where user can check current state of the form and how the Movie will appear in the Movie List */}
         <Movie />
       </section>
-      <MovieList />
+
+      {/*List of Movies at bottom of the page where user can view, filter, or delete previously added movies*/}
+      <section className='viewMovies'>
+
+        {/*Input field where the user can edit state (filterText) in order to filter the movies that display, matches on Title using USEEFFECT above*/}
+        <input value={filterText} onChange={(e => setFilterText(e.target.value))} />
+        
+        {/*Display list of all movies previously added, user is able to delete a movie by clicking on the div*/}
+        <MovieList />
+      </section>
     </div>
   );
 }
