db613d5a936298015e56c4ffc0f8a38b71ed203f
Will Gundy <willgunderson503@gmail.com>
2022-05-05 10:14:42 -0700

    quick fix on props in component pages

--- src/Movie.js
+++ src/Movie.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Movie(props) {
   return (
-    <div>Movie</div>
+    <div>
+        
+    </div>
   );
 }
--- src/MovieForm.js
+++ src/MovieForm.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function MovieForm(props) {
   return (
-    <div>Movie</div>
+    <div>
+      
+    </div>
   );
 }
